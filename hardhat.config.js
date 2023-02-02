15845fca02fe48a7f625653ba3852fd88811841b
waqeen <a.zhdanuhin@mail.ru>
2023-02-03 01:51:33 +0300

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {}
+  },
 };
